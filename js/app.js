3ff5ece78fef6aff99c2efee4984fde0bf9a4b31
Dontyouwatch <97377754+Dontyouwatch@users.noreply.github.com>
2025-08-31 22:24:12 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Fetch and insert the header
-  fetch('header.html')
+  fetch('/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header-placeholder').innerHTML = data;
     });
 
   // Fetch and insert the footer
-  fetch('footer.html')
+  fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer-placeholder').innerHTML = data;
